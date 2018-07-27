0ea16c52960773245fb9f24f8965f5ffba384d17
osparking <jbpark103@gmail.com>
2018-07-27 09:31:59 +0900

    Ch 11. slide up slide down elements

---
+++ js/ch11_1.js
@@ -0,0 +1,10 @@
+/* global document, alert */
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+var btn = $("button#make_visible");
+btn.on("click", function () {
+  alert("버튼 클릭됨");
+});
